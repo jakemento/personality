fcb2208651c6f7c22e568b0d88e8f6f71e129393
Jake Steinberg <student@epicodus.com>
2016-01-15 14:05:12 -0800

    pushing to github

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vacation Generator Quiz
+####website building exercise for Epicodus, 01.08.2015
+###By Jacob Steinberg
+##Description
+This is a website I created for my second code review. Its main function is to suggest a vacation destination based upon a series of questions. This web site is an exercise in HTML, CSS, Javascript and jquery.
+##Program Setup Instructions
+navigate to the site, and select answers for the questions whem promnpted, then you will see your vacation recommendation.
+###Link
+http://jakemento.github.io/personality/
+##Legal
+Copyright(c) 2015 Jacob Steinberg
+
---
+++ js/scripts.js
@@ -0,0 +1,214 @@
+$(document).ready(function() {
+$(function(){
+  $('button').click(function(){
+    $('img').each(function(item){
+      if (item>-1){
+        $(this).hide();
+      }
+    });
+  });
+});
+
+
+$(function(){
+  $('button').click(function(){
+    $('#img2').show();
+  });
+});
+
+$(function(){
+  $('button').click(function(){
+    $('#img3').show();
+  });
+});
+
+
+
+
+
+$(function(){
+  $('button').click(function(){
+    $('h2').show();
+  });
+});
+
+$(function(){
+  $('button').click(function(){
+    $('h3').show();
+  });
+});
+
+
+
+
+
+$(function(){
+  $('button').click(function(){
+    $('#img4').show();
+  });
+});
+
+$(function(){
+  $('button').click(function(){
+    $('#formid1').show();
+  });
+});
+
+$(function(){
+  $('button').click(function(){
+    $('#formid1').show();
+  });
+});
+
+$(function(){
+  $('.btn2').click(function(){
+    $('.btn2').hide();
+  });
+});
+
+
+$("form#formid1").submit(function(event) {
+
+var age = parseInt($("input#age").val());
+var food = $("select#food").val();
+var weather = $("select#weather").val();
+var lodgings = $("select#lodgings").val();
+var activities = $("select#activities").val();
+
+if (food === "poi" && weather === "hot" && lodgings === "resort" && activities === "snorkel" && age<=100) {
+  $('#Hawaii').show();
+  $('.img2').hide();
+  $('.img4').hide();
+}
+
+if (food === "poi" && weather === "warm") {
+  $('#Hawaii').show();
+  $('.img2').hide();
+  $('.img4').hide();
+} else {
+  $('#Hawaii').hide();
+}
+
+if (food === "paella" && weather === "warm" && lodgings === "gothic" && activities === "cathedral" && age<=40) {
+  $('#Barcelona').show();
+  $('.img2').hide();
+  $('.img3').hide();
+}
+
+if (food === "paella" && weather === "warm" && lodgings === "ice" && activities === "skiing") {
+  $('#Barcelona').show();
+  $('.img2').hide();
+  $('.img3').hide();
+}
+
+if (food === "paella" && weather === "warm" && age>=0) {
+  $('#Barcelona').show();
+  $('.img2').hide();
+  $('.img3').hide();
+}
+
+if (food === "paella" && lodgings === "gothic") {
+  $('#Barcelona').show();
+  $('.img2').hide();
+  $('.img3').hide();
+}
+
+if (food === "paella" && activities === "cathedral") {
+  $('#Barcelona').show();
+  $('.img2').hide();
+  $('.img3').hide();
+}
+
+
+
+if (food === "paella" && weather === "warm") {
+  $('#Barcelona').show();
+  $('.img2').hide();
+  $('.img3').hide();
+}
+
+if (food === "fondue" && weather === "cold") {
+  $('#Switzerland').show();
+  $('.img3').hide();
+  $('.img4').hide();
+}
+
+if (food === "poi" && weather === "warm") {
+  $('#Hawaii').show();
+  $('.img2').hide();
+  $('.img4').hide();
+} else {
+  $('#Hawaii').hide();
+}
+
+
+if (activities === "skiing" && weather === "cold") {
+  $('#Switzerland').show();
+  $('.img3').hide();
+  $('.img4').hide();
+}
+
+if (lodgings === "ice" && weather === "cold") {
+  $('#Switzerland').show();
+  $('.img3').hide();
+  $('.img4').hide();
+}
+
+if (lodgings === "ice" && weather === "warm") {
+  $('#Switzerland').show();
+  $('.img3').hide();
+  $('.img4').hide();
+}
+
+if (food === "poi" && weather === "hot") {
+  $('#Hawaii').show();
+  $('.img2').hide();
+  $('.img4').hide();
+}
+
+if (weather === "hot" && lodgings === "resort") {
+  $('#Hawaii').show();
+  $('.img2').hide();
+  $('.img4').hide();
+}
+
+if (age <=100 && weather === "warm" && food === "paella") {
+  $('#Barcelona').show();
+  $('.img2').hide();
+  $('.img3').hide();
+}
+
+if (lodgings === "ice" && activities === "skiing") {
+  $('#Switzerland').show();
+  $('.img3').hide();
+  $('.img4').hide();
+}
+
+if (lodgings === "gothic" && activities === "cathedral") {
+  $('#Barcelona').show();
+  $('.img2').hide();
+  $('.img3').hide();
+}
+
+if (weather === "cold" && lodgings === "cathedral") {
+  $('#Barcelona').show();
+  $('.img2').hide();
+  $('.img3').hide();
+}
+
+if (weather === "warm" && lodgings === "gothic" && activities === "snorkeling") {
+  $('#Barcelona').show();
+  $('.img2').hide();
+  $('.img3').hide();
+}
+
+if (food === "paella" && weather === "warm" && lodgings === "ice" && activities === "skiing") {
+  $('#Barcelona').show();
+  $('.img3').hide();
+}
+
+
+
+event.preventDefault();
+});
+});
