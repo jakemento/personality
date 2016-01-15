0a0073fee0ec367ea91a47aeac0a27797a01a33e
Jacob Steinberg <student@epicodus.com>
2016-01-15 08:57:43 -0800

    add second class of images after click

--- js/scripts.js
+++ js/scripts.js
@@ -34,45 +34,44 @@ $(function(){
 });
 
 
-// ==================================================================
 
-$("form#formid1").submit(function(event) {
-
-var age = parseInt($("input#age").val());
-var food = $("select#food").val();
-
-if (food === "Poi" && age >14 "Birdperson" && alien !== "Zeke" && age>50 && musical !=='Schwifty') {
-  $('#rick').show();
-} else {
-  $('#rick').hide();
-}
-
-if (alien !== "Gearhead" && alien === "Birdperson" && alien !== "Zeke" && age>50 && musical !=='Schwifty') {
-  $('#morty').show();
-} else {
-  $('#morty').hide();
-}
-
-if (alien !== "Gearhead" && alien !== "Birdperson" && alien === "Zeke" && age>50 && musical !=='Schwifty') {
-  $('#jerry').show();
-} else {
-  $('#jerry').hide();
-}
-if (age<=50 && age>= 31 && (musical !=='Schwifty')) {
-  $('#beth').show();
-} else {
-  $('#beth').hide();
-}
-if (age>=1 && age<=30 && (musical !=='Schwifty')) {
-  $('#summer').show();
-} else {
-  $('#summer').hide();
-}
-if (age>0 && musical ==='Schwifty') {
-  $('#meseeks').show();
-} else {
-  $('#meseeks').hide();
-}
+//
+// $("form#formid1").submit(function(event) {
+//
+// var age = parseInt($("input#age").val());
+// var food = $("select#food").val();
+//
+// if (food === "Poi" && age >14 "Birdperson" && alien !== "Zeke" && age>50 && musical !=='Schwifty') {
+//   $('#rick').show();
+// } else {
+//   $('#rick').hide();
+// }
+//
+// if (alien !== "Gearhead" && alien === "Birdperson" && alien !== "Zeke" && age>50 && musical !=='Schwifty') {
+//   $('#morty').show();
+// } else {
+//   $('#morty').hide();
+// }
+//
+// if (alien !== "Gearhead" && alien !== "Birdperson" && alien === "Zeke" && age>50 && musical !=='Schwifty') {
+//   $('#jerry').show();
+// } else {
+//   $('#jerry').hide();
+// }
+// if (age<=50 && age>= 31 && (musical !=='Schwifty')) {
+//   $('#beth').show();
+// } else {
+//   $('#beth').hide();
+// }
+// if (age>=1 && age<=30 && (musical !=='Schwifty')) {
+//   $('#summer').show();
+// } else {
+//   $('#summer').hide();
+// }
+// if (age>0 && musical ==='Schwifty') {
+//   $('#meseeks').show();
+// } else {
+//   $('#meseeks').hide();
+// }
 event.preventDefault();
-});
 });
